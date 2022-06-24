4443de1db0a5b41f4eac4010d86fa9f09bb245a4
unknown <lribeirolaura@github.com>
2022-06-23 23:10:39 -0300

    feat: add routes and new components to the app

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,50 +1,54 @@
-import {Event} from './Pages/Event';
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter} from "react-router-dom";
+import { client } from "./lib/apollo";
+import { Event } from "./Pages/Event";
+import Router from "./Router";
 
-function App ( ){
-
+function App() {
   return (
-    <div>
-      <Event />
-    </div>
-  )
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
 
+// import { gql, useQuery } from "@apollo/client";
+// import { useEffect } from "react";
+// //gql é uma função do graphQL que permite fazer a requisição com destaque e diferenciação para cada query realizada
+// const GET_LESSONS_QUERY = gql`
+//   query {
+//     lessons {
+//       id
+//       title
+//     }
+//   }
+// `
 
-  // import { gql, useQuery } from "@apollo/client";
-  // import { useEffect } from "react";
-  // //gql é uma função do graphQL que permite fazer a requisição com destaque e diferenciação para cada query realizada
-  // const GET_LESSONS_QUERY = gql`
-  //   query {
-  //     lessons {
-  //       id
-  //       title
-  //     }
-  //   }
-  // `
-  
-  // interface Lesson {
-  //   id: string;
-  //   title: string;
-  // }
-  
-  // function App() {
-  //   const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-  
-  //   // useEffect(() => {
-  //   //   	client.query({
-  //   //       query: GET_LESSONS_QUERY
-  //   //     }).then(response => console.log(response.data))
-  //   // }, [])
-  
-  //   return (
-  //     <ul>
-  //       {data?.lessons.map(lesson => {
-  //         return <li key={lesson.id}>{lesson.title}</li>
-  //       })}
-  //     </ul>
-  //   )
-  // }
-  
-  // export default App;
+// interface Lesson {
+//   id: string;
+//   title: string;
+// }
+
+// function App() {
+//   const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+
+//   // useEffect(() => {
+//   //   	client.query({
+//   //       query: GET_LESSONS_QUERY
+//   //     }).then(response => console.log(response.data))
+//   // }, [])
+
+//   return (
+//     <ul>
+//       {data?.lessons.map(lesson => {
+//         return <li key={lesson.id}>{lesson.title}</li>
+//       })}
+//     </ul>
+//   )
+// }
+
+// export default App;
--- event-platform/src/components/Sidebar.tsx
+++ event-platform/src/components/Sidebar.tsx
@@ -1,6 +1,35 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
+
+const GET_LESSONS_QUERY = gql`
+query MyQuery {
+  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+    id
+    title
+    slug
+    availableAt
+    lessonType
+  }
+}`
+
+//cria tipagem do retorno da QUERY 
+
+interface GetLessonsQueryResponse {
+  lessons: Array<{
+    id: string, 
+    title: string, 
+    slug: string, 
+    availableAt: string, 
+    lessonType: 'class' | 'live'
+  }>
+}
+
+
 export function Sidebar() {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY); 
+  console.log(data); 
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
@@ -8,12 +37,19 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        <Lesson /> 
-        <Lesson /> 
-        <Lesson /> 
-        <Lesson /> 
-        <Lesson /> 
-        <Lesson /> 
+      {
+        data?.lessons.map(lesson => {
+          return (
+            <Lesson 
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType}
+            />
+          )
+        })
+      }
       </div>
     </aside>
   );
